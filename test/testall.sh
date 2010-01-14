3974997444fa83e68f307cb7e918caea4068e5fb
Klaus Kämpf <kkaempf@suse.de>
2010-01-14 11:29:15 +0100

    Exit loop on error

--- test/testall.sh
+++ test/testall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 for i in $@; do
-  echo $i
+  echo -n $i " "
   ruby ../src/rbmof.rb -q -s wmi -I /usr/share/mof/cim-current ../mof/wmi-qualifiers.mof $i
+  if [ $? -ne 0 ]; then
+    break
+  fi
 done
