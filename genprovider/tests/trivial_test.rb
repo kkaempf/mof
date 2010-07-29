025e02fc3f772f63d613211ad3ffde6a2925b06e
Klaus Kämpf <kkaempf@suse.de>
2010-07-29 12:43:28 +0200

    trivial_test: write output to tests directory

--- genprovider/tests/trivial_test.rb
+++ genprovider/tests/trivial_test.rb
@@ -1,9 +1,14 @@
 require "test/unit"
+
 PARENTNAME = File.join(File.dirname(__FILE__),"..")
 GENPROVIDER = File.expand_path(File.join(PARENTNAME,"genprovider.rb"))
+MOFNAME = File.join(File.dirname(__FILE__),"trivial.mof")
+OUTPUT = File.join(File.dirname(__FILE__),"output.rb")
+
 def run_genprovider
-  system "ruby", GENPROVIDER, File.join(File.dirname(__FILE__),"trivial.mof")
+  system "ruby", GENPROVIDER, "-o", OUTPUT, MOFNAME
 end
+
 class TrivialTest < Test::Unit::TestCase
   def test_running_genprovider
     run_genprovider
