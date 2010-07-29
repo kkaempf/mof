6c5d3f4cdd4817232a3140daccc6ba33c3d504af
Klaus Kämpf <kkaempf@suse.de>
2010-07-29 08:31:49 +0200

    Initial test - wip

---
+++ genprovider/tests/trivial_test.rb
@@ -0,0 +1,10 @@
+require "test/unit"
+parentname = File.join(File.dirname(__FILE__),"..")
+def run_genprovider
+end
+class TrivialTest < Test::Unit::TestCase
+  test 'running genprovider' do
+  end
+end
+
+  
